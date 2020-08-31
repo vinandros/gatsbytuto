f9c04ff225ca53f468f879a11916280eb850bfdf
vinandros <vinandros@gmail.com>
2020-08-31 10:29:39 -0600

    part 5 graphiQL

---
+++ dist/gatsby-config.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: "Pandas Eating Lots"
+  },
+  plugins: [{
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "src",
+      path: "".concat(__dirname, "/src/")
+    }
+  }, "gatsby-plugin-emotion", {
+    resolve: "gatsby-plugin-typography",
+    options: {
+      pathToConfigModule: "src/utils/typography"
+    }
+  }]
+};
